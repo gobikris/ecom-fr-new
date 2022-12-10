26d3a25a041ec61fd366541e084fc27ec97592bb
gobikris <96045276+gobikris@users.noreply.github.com>
2022-12-10 16:44:48 +0530

    Update auth.service.js

--- src/auth.service/auth.service.js
+++ src/auth.service/auth.service.js
@@ -3,4 +3,3 @@
 // export const API_URL = "http://localhost:3002";
 
 // export const API_URL = "https://urbanclothes.herokuapp.com"
-export const API_URL = "https://n-back-end-gobikris.vercel.app/"
