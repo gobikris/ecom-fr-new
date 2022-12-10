61f772fc3cbe2cad897aaaa89f2a48ff6b1013d0
gobikris <gobikrishna005@gmail.com>
2022-12-10 19:40:59 +0530

    u

--- src/auth.service/auth.service.js
+++ src/auth.service/auth.service.js
@@ -4,4 +4,4 @@
 
 // export const API_URL = "https://urbanclothes.herokuapp.com"
 
-export const API_URL = "https://back-end-iu7l-72r5c3o86-gobikris.vercel.app/
+export const API_URL = "https://back-end-iu7l-72r5c3o86-gobikris.vercel.app/"
