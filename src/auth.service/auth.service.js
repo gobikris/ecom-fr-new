c1a1789d97e86dda25e96a21bb036dd6fa1a5c39
gobikris <gobikrishna005@gmail.com>
2022-12-11 14:37:47 +0530

    ua

--- src/auth.service/auth.service.js
+++ src/auth.service/auth.service.js
@@ -1,5 +1,5 @@
 
 
-// export const API_URL = "http://localhost:3002";
-// export const API_URL = "https://back-end-iu7l-72r5c3o86-gobikris.vercel.app/"
-export const API_URL = "https://ill-ruby-raven-hat.cyclic.app"
+// export const API_URL = "http://localhost:8292";
+export const API_URL = "https://back-end-iu7l-72r5c3o86-gobikris.vercel.app/"
+// export const API_URL = "https://back-end-iu7l.vercel.app/"
