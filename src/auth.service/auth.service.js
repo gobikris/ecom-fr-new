0173bb2775892e54b9534d6f366d9f9996175def
gobikris <gobikrishna005@gmail.com>
2022-12-11 13:50:30 +0530

    c

--- src/auth.service/auth.service.js
+++ src/auth.service/auth.service.js
@@ -1,4 +1,5 @@
 
 
 // export const API_URL = "http://localhost:3002";
-export const API_URL = "https://back-end-iu7l-72r5c3o86-gobikris.vercel.app/"
+// export const API_URL = "https://back-end-iu7l-72r5c3o86-gobikris.vercel.app/"
+export const API_URL = "https://ill-ruby-raven-hat.cyclic.app"
