838db24c3d91b12c4579aaec5359e812a78616a3
gobikris <gobikrishna005@gmail.com>
2022-12-22 08:05:40 +0530

    api

--- src/auth.service/auth.service.js
+++ src/auth.service/auth.service.js
@@ -1,4 +1,4 @@
 
-export const API_URL = "https://back-end-iu7l.vercel.app/"
+export const API_URL = "https://back-end-iu7l-7recylij4-gobikris.vercel.app/"
 // export const API_URL = "http://localhost:8292";
 
